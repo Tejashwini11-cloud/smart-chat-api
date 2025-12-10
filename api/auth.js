7a1a9ce677f4f08d351f23e9bb69136511326bd2
Tejashwini <tejashwiniash11@gmail.com>
2025-12-11 00:00:31 +0530

    update api/auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,17 +1,3 @@
-import { google } from "googleapis";
-
-export default async function handler(req, res) {
-  try {
-    const client = new google.auth.OAuth2(
-      process.env.CLIENT_ID,
-      process.env.CLIENT_SECRET,
-      process.env.REDIRECT_URI
-    );
-
-    const { tokens } = await client.getToken(req.query.code);
-    res.status(200).json(tokens);
-
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+export default function handler(req, res) {
+  res.status(200).json({ message: "Auth route is alive!" });
 }
